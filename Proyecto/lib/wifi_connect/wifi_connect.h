ac0a80ed0446ef4d878518bfb112fb11bc0aca84
Roberto-uni <roberto.enrique.dos.ramos@estudiantat.upc.edu>
2025-06-02 02:38:05 +0200

    Update wifi_connect.h

--- Proyecto/lib/wifi_connect/wifi_connect.h
+++ Proyecto/lib/wifi_connect/wifi_connect.h
@@ -1,8 +1,12 @@
 #pragma once
 #include <WiFi.h>
+#include "Pantalla_SPI.h"
+
+// Declaración anticipada para evitar incluir Pantalla_SPI.h aquí
+class Pantalla_SPI;
 
 class wifi_connect {
 public:
-    void conectar(const char* ssid, const char* password);
+    void conectar(const char* ssid, const char* password, Pantalla_SPI& tft);
     bool estaConectado();
 };
