1e0e304d6e8fd613f1a52c9079093c9b4c5249f4
Roberto-uni <roberto.enrique.dos.ramos@estudiantat.upc.edu>
2025-06-02 02:37:44 +0200

    Update wifi_connect.cpp

--- Proyecto/lib/wifi_connect/wifi_connect.cpp
+++ Proyecto/lib/wifi_connect/wifi_connect.cpp
@@ -1,8 +1,11 @@
 #include "wifi_connect.h"
+#include "Pantalla_SPI.h"
 
-void wifi_connect::conectar(const char* ssid, const char* password) {
+
+void wifi_connect::conectar(const char* ssid, const char* password, Pantalla_SPI& tft) {
     WiFi.begin(ssid, password);
     Serial.print("Conectando a WiFi");
+    tft.mostrarTexto("Conectando a WiFi", 20);
     while (WiFi.status() != WL_CONNECTED) {
         delay(500);
         Serial.print(".");
