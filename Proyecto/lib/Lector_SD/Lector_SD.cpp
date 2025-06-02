dc9dab411c1e60295ffb5392fca8d074e1f08df7
Roberto-uni <roberto.enrique.dos.ramos@estudiantat.upc.edu>
2025-06-02 02:35:01 +0200

    Update Lector_SD.cpp

--- Proyecto/lib/Lector_SD/Lector_SD.cpp
+++ Proyecto/lib/Lector_SD/Lector_SD.cpp
@@ -1,19 +1,40 @@
 #include "Lector_SD.h"
 
+SPIClass SPI_SD(FSPI);  // FSPI equivale a SPI3 en ESP32-S3
 
 bool Lector_SD::iniciar() {
-    //SPI.begin(PIN_SCLK, PIN_MISO, PIN_MOSI);
-    if (!SD.begin(SD_CS)) {
-        Serial.println("Error al inicializar la tarjeta SD");
+    Serial.println("Inicializando SPI3 y SD...");
+
+    SPI_SD.begin(PIN_SPI_SCK, PIN_SPI_MISO, PIN_SPI_MOSI, PIN_SPI_CS);
+    if (!SD.begin(PIN_SPI_CS, SPI_SD)) {
+        Serial.println("¡Fallo al montar la tarjeta SD!");
         return false;
     }
-    Serial.println("SD inicializada correctamente");
+
+    Serial.println("Tarjeta SD montada.");
     return true;
 }
+
+std::vector<String> Lector_SD::obtenerListaCanciones() {
+    canciones.clear();
+    File root = SD.open("/");
+    while (File file = root.openNextFile()) {
+        String nombre = file.name();
+        if (nombre.endsWith(".mp3")) {
+            canciones.push_back(nombre);
+        }
+        file.close();
+    }
+    return canciones;
+}
+
+
 
 File Lector_SD::obtenerCancion(const String& nombreArchivo) {
     return SD.open("/" + nombreArchivo);
 }
+
+
 
 void Lector_SD::listarCanciones() {
     File root = SD.open("/");
@@ -22,4 +43,5 @@ void Lector_SD::listarCanciones() {
         file.close();
     }
 }
+
 
