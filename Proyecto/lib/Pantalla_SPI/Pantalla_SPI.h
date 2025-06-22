1b5eb8fb3ab3f8e92db1105d2678f7eef304e7b8
Roberto-uni <roberto.enrique.dos.ramos@estudiantat.upc.edu>
2025-06-23 01:00:11 +0200

    Update Pantalla_SPI.h

--- Proyecto/lib/Pantalla_SPI/Pantalla_SPI.h
+++ Proyecto/lib/Pantalla_SPI/Pantalla_SPI.h
@@ -9,7 +9,7 @@ public:
     void mostrarNombreEstacion(const String& nombre);
     void mostrarNombreCancion(const String& nombre);
 
-    void mostrarTiempo(uint32_t actual, uint32_t total);
+    void mostrarTiempo(uint32_t actual);
     void mostrarVolumen(float volumen);
     void mostrarEstadoReproduccion(bool pausado);
 
