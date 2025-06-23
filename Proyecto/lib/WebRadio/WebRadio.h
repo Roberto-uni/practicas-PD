6aa5e038002bb037759809893c57b3293b1f5e90
Roberto-uni <roberto.enrique.dos.ramos@estudiantat.upc.edu>
2025-06-23 23:47:32 +0200

    Update WebRadio.h

--- Proyecto/lib/WebRadio/WebRadio.h
+++ Proyecto/lib/WebRadio/WebRadio.h
@@ -1,12 +1,24 @@
-#pragma once
+#ifndef WEBRADIO_H
+#define WEBRADIO_H
 
+#include <WebServer.h>
+#include "Lector_SD.h"
 
 class WebRadio {
 public:
-    WebRadio(const char* url);
+    // Recibe referencia a Lector_SD externo y puerto del servidor (default 80)
+    WebRadio(Lector_SD& lectorSd, uint16_t puerto = 80);
 
+    void iniciar();
+    void manejarClientes();
 
 private:
-    const char* streamURL;
+    WebServer server;
+    Lector_SD& lector;  // Referencia a objeto externo
 
+    void manejarRaiz();
+    void manejarStream();
 };
+
+#endif // WEBRADIO_H
+
